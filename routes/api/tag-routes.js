33322381db388f8bc8af8e02113c326339bc5a1e
Matthew Wiser <matt.wiser@icloud.com>
2022-07-14 01:50:31 -0500

    finished route and server functionality with db sync

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,25 +4,107 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+    .then(tagRow => res.json(tagRow))
+    .catch(err => {
+      console.log(err);
+      res.ststus(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+    .then(tagRow => {
+      if (!tagRow) {
+        res.status(404).json({ message: 'That tag does not exist!' });
+        return;
+      }
+      res.json(tagRow);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(tagRow => res.json(tagRow))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    { 
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(tagRow => {
+    if (!tagRow[0]) {
+      res.status(404).json({ message: 'That tag does not exist!'});
+      return;
+    }
+    res.json(tagRow)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagRow => {
+    if (!tagRow) {
+      res.status(404).json({ message: 'That tag does not exist!' });
+      return;
+    }
+    res.json(tagRow);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 module.exports = router;
